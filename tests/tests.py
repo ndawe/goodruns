8d37bd19873abdae3355f4f48c4cbe5e692c1dcf
Noel Dawe <Noel.Dawe@cern.ch>
2011-09-17 22:43:33 -0700

    updates

--- tests/tests.py
+++ tests/tests.py
@@ -2,10 +2,12 @@ import nose
 import os
 from goodruns import GRL
 
-def test_read():
+def test():
     
     dirname = os.path.dirname(__file__)
     grl = GRL(os.path.join(dirname, "grl.xml"))
+
+    assert (180225, 87) in grl
 
 if __name__ == '__main__':
     nose.runmodule()
