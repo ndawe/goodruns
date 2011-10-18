09d373e865ffef7f0e6f86298c5b25c8e25bea97
Noel Dawe <Noel.Dawe@cern.ch>
2011-10-18 16:18:55 -0700

    updates

--- tests/tests.py
+++ tests/tests.py
@@ -2,18 +2,28 @@ import nose
 import os
 from goodruns import GRL
 
-def test():
+DIRNAME = os.path.dirname(__file__)
+
+def str_init_test():
     
-    dirname = os.path.dirname(__file__)
-    grl = GRL(os.path.join(dirname, "grl.xml"))
+    grl = GRL(os.path.join(DIRNAME, 'grl.xml'))
 
     assert (180225, 87) in grl
+
 
 def dict_init_test():
 
     a = {1234: [(1,2), (4,5)]}
     grl = GRL(a)
     assert (1234, 1) in grl
+
+
+def file_init_test():
+    
+    with open(os.path.join(DIRNAME, 'grl.xml')) as f:
+        grl = GRL(f)
+        assert (180225, 87) in grl
+
 
 if __name__ == '__main__':
     nose.runmodule()
