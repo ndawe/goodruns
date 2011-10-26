74439d47579f4d201bfdcc9012909f05e2358a87
Noel Dawe <Noel.Dawe@cern.ch>
2011-10-26 11:35:14 -0700

    updates

--- tests/speed.py
+++ tests/speed.py
@@ -6,6 +6,9 @@ from ROOT import Root
 import random
 from goodruns import GRL
 from numpy import random
+import time
+import sys
+
 
 reader = Root.TGoodRunsListReader('grl.xml')
 reader.Interpret()
@@ -13,22 +16,33 @@ goodrunslist = reader.GetMergedGRLCollection()
 
 grl = GRL('grl.xml')
 
-size = 10000000
+size = 5000000
 runs = [int(i) for i in random.randint(178000, 190500, size=size)]
 lbs = [int(i) for i in random.randint(0, 1000, size=size)]
+
 
 def GoodRunsLists():
     for i in xrange(size):
         goodrunslist.HasRunLumiBlock(runs[i], lbs[i])
 
+
 def goodruns():
     for i in xrange(size):
         (runs[i], lbs[i]) in grl
 
-import time
+
+print "Running over %i random (run, lumiblock) combinations..." % size
+
+print "GoodRunsLists... ",
+sys.stdout.flush()
 t1 = time.time()
 GoodRunsLists()
-print time.time() - t1
+t2 = time.time()
+print "%f [sec]" % (t2 - t1)
+
+print "goodruns... ",
+sys.stdout.flush()
 t1 = time.time()
 goodruns()
-print time.time() - t1
+t2 = time.time()
+print "%f [sec]" % (t2 - t1)
