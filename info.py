e571726605cf7862ee973ec2b5aabd50e0010b28
Noel <Noel.Dawe@cern.ch>
2011-10-27 16:45:11 -0700

    new version

--- info.py
+++ info.py
@@ -1,4 +1,4 @@
-__VERSION__ = '1.0'
+__VERSION__ = '1.5'
 __URL__ = 'http://ndawe.github.com/goodruns'
 __GIT_URL__ = 'https://github.com/ndawe/goodruns/'
 __DOWNLOAD_URL__ = 'http://pypi.python.org/packages/source/g/goodruns/goodruns-%s.tar.gz' % __VERSION__
