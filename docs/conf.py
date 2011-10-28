e6c716ba8e6322a1b9e300d8f927cf79cba28dd8
Noel Dawe <Noel.Dawe@cern.ch>
2011-10-27 18:49:23 -0700

    updates

--- docs/conf.py
+++ docs/conf.py
@@ -48,9 +48,9 @@ copyright = u'2011, Noel Dawe'
 # built documents.
 #
 # The short X.Y version.
-version = '1.0'
+version = '1.5'
 # The full version, including alpha/beta/rc tags.
-release = '1.0'
+release = '1.5'
 
 autoclass_content = "both"
 
